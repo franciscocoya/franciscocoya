d91b9ffffa0a3c380d7d11bb0310d62565cde3c4
franciscocoya <kikocoya@gmail.com>
2023-07-16 10:04:48 +0200

    style: add website owner

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import styles from './footer.module.scss'
 function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>&copy; {new Date().getFullYear()} All Rights Reserved.</p>
+      <p>&copy; {new Date().getFullYear()} <i>Francisco Coya</i>. All Rights Reserved.</p>
     </footer>
   )
 }

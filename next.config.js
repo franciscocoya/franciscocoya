2f1669030cf876a4fa881ec87f4a57747c022444
franciscocoya <kikocoya@gmail.com>
2023-09-25 11:42:27 +0200

    security: add security headers and security.txt file

--- next.config.js
+++ next.config.js
@@ -2,16 +2,60 @@
 const path = require('path')
 
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'same-origin',
+          },
+          {
+            key: 'Feature-Policy',
+            value: 'camera "none"; microphone "none"; geolocation "none";',
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(), microphone=(), geolocation=()',
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains; preload',
+          },
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          }
+        ],
+      },
+    ]
+  },
+
   i18n: {
     locales: ['en-GB', 'es-ES'],
     defaultLocale: 'en-GB',
     localeDetection: false,
   },
+
   sassOptions: {
     includePaths: [path.join(__dirname, '_styles')],
   },
 
   trailingSlash: true,
+  poweredByHeader: false,
 }
 
 module.exports = nextConfig
